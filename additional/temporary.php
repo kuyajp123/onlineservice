f77ab899eba2c4d2c7f3239f75c6a024151a9280
kuyajp123 <kuyajp123@gmail.com>
2024-07-19 23:23:56 +0800

    New

--- additional/temporary.php
+++ additional/temporary.php
@@ -1,39 +1,147 @@
+            <!---eto ang simula ng ipapasok mo sa loob ng div sa may comment--------->
+            <div class="container-fluid container_post">
 
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
-a<br>
+                <!-- lagayan ng pangalan 1st div -->
+                <div class="container-fluid name">
+                    
+                    <div class="container-fluid lgyan">
+                      
+                      <div class="container-fluid prof_pic">
+                        <!-- profile image inpost -->
+                          <div><a href=""><img src="include/images/profile.jpg" style="object-fit:contain; width: 40px;
+    height: 40px; border-radius: 50%;" alt=""></div></a>
+                      </div>
+                      <div class="container-fluid pangalan">
+                        <!-- profile name in post -->
+                        <div><a href="" style="font-size:1rem; text-decoration: none; color: black;">John Paul Naag</span></a>
+                        <!-- time in post -->
+                        <div><small style="font-size:13px;">Just now</small></div></div></div>
+                      </div>
+                      
+
+                    <!-- 3dots 1st div -->
+                    <div class="container-fluid dots">
+                      <div class="container-fluid dot"><i class="fa-solid fa-ellipsis fa-xl" style="color: #575b60; font-size:20px;"></i></div>
+                    </div>
+                </div>
+
+                <!-- image 1st div -->
+                <div class="container-fluid image">
+                <!-- image in post -->
+                <div class="container-fluid image_container"><img src="include/images/profile.jpg" alt=""></div>
+                    
+                </div>
+
+
+                <!-- heart,comment,share, 1st div -->
+                <div class="container-fluid heart">
+                   
+                    <div class="container-fluid thethree">
+                      <div class="container-fluid puso"><i class="fa-regular fa-heart"></i></div>
+                      <div class="container-fluid comment"><i class="fa-regular fa-comment-dots fa-flip-horizontal"></i></div>
+                      <div class="container-fluid share"><i class="fa-regular fa-paper-plane"></i></div>
+                    </div>
+
+                    <!-- collection 1st div -->
+                    <div class="container-fluid collection">
+                      <div class="container-fluid save">
+                        <div class="container-fluid bookmarkicon">
+                          <i class="fa-regular fa-bookmark"></i>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+
+                      <!-- line -->
+                      <div class="line"></div>
+
+                <!-- caption 1st div -->
+                <div class="container-fluid container_cap">
+                    <div class="container-fluid caption">This is post with image</div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+<!-- -------------------------------second post ---------------------------------------------->
+
+
+
+
+
+
+
+
+
+
+
+
+
+             <!---eto ang simula ng ipapasok mo sa loob ng div sa may comment--------->
+            <div class="container-fluid container_post">
+
+<!-- lagayan ng pangalan 1st div -->
+<div class="container-fluid name">
+    
+    <div class="container-fluid lgyan">
+      
+      <div class="container-fluid prof_pic">
+        <!-- profile image inpost -->
+          <div><a href=""><img src="include/images/profile.jpg" style="object-fit:contain; width: 40px;
+height: 40px; border-radius: 50%;" alt=""></div></a>
+      </div>
+      <div class="container-fluid pangalan">
+        <!-- profile name in post -->
+        <div><a href="" style="font-size:1rem; text-decoration: none; color: black;">John Paul Naag</span></a>
+        <!-- time in post -->
+        <div><small style="font-size:13px;">Just now</small></div></div></div>
+      </div>
+      
+
+    <!-- 3dots 1st div -->
+    <div class="container-fluid dots">
+      <div class="container-fluid dot"><i class="fa-solid fa-ellipsis fa-xl" style="color: #575b60; font-size:20px;"></i></div>
+    </div>
+</div>
+
+<!-- image 1st div -->
+<div class="container-fluid image">
+<!-- image in post -->
+<div class="container-fluid image_container"><img src="include/images/Home (1).png" alt=""></div>
+    
+</div>
+
+
+<!-- heart,comment,share, 1st div -->
+<div class="container-fluid heart">
+   
+    <div class="container-fluid thethree">
+      <div class="container-fluid puso"><i class="fa-regular fa-heart"></i></div>
+      <div class="container-fluid comment"><i class="fa-regular fa-comment-dots fa-flip-horizontal"></i></div>
+      <div class="container-fluid share"><i class="fa-regular fa-paper-plane"></i></div>
+    </div>
+
+    <!-- collection 1st div -->
+    <div class="container-fluid collection">
+      <div class="container-fluid save">
+        <div class="container-fluid bookmarkicon">
+          <i class="fa-regular fa-bookmark"></i>
+        </div>
+      </div>
+    </div>
+</div>
+
+      <!-- line -->
+      <div class="line"></div>
+
+<!-- caption 1st div -->
+<div class="container-fluid container_cap">
+    <div class="container-fluid caption">This is post with image</div>
+</div>
+
+</div>
