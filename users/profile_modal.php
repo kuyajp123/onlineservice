ece01b19983180fb44381fc4e138d6c8eed47258
kuyajp123 <kuyajp123@gmail.com>
2024-07-22 14:12:57 +0800

    New

--- users/profile_modal.php
+++ users/profile_modal.php
@@ -9,15 +9,31 @@
             <div class="container-fluid p-0 m-0 bodymodalprof">
                 <div class="container-fluid coverphotoprof">
                   <div class="container-fluid coverphoto">
-                    <div class="container-fluid imgcoverdiv"><img src="include/images/433610472_1427797164763718_6516813264608892964_n.jpg" alt=""></div>
-                  
+                    <!-- upload cover photo here -->
+                    <div class="container-fluid imgcoverdiv"><img src="include/images/0wzknw7460n31.jpg" alt=""></div>
                     <div class="container-fluid coverphotoblurd"></div>
-                    <div class="container-fluid profilemodal"></div>
+                    <div class="container-fluid profilemodal">
+                      <!-- upload profile photo here -->
+                        <div class="container-fluid modalprofcon"><img src="include/images/433610472_1427797164763718_6516813264608892964_n.jpg" alt=""></div>
+                    </div>
                     <button type="button" class="btn-close btncloseprof" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <!-- <div class="container-fluid profilemodal"></div> -->
                 </div>
-                <div class="container-fluid nbp"></div>
+                <div class="container-fluid nbp">
+                    <div class="container-fluid namebiomodalprof">
+                      <!-- name/bio here -->
+                      <div class="container-fluid namemodalprof">John Paul Naag</div>
+                      <div class="container-fluid biomodalprof">Hello world</div>
+                    </div>
+                    <div class="container-fluid buttonmodalprof">
+                      <div class="container-fluid profbuttonmodal">
+                        <!-- profile here -->
+                      <a href=""><button class="btn">
+                      Profile</button></a>
+                      </div>
+                    </div>
+                </div>
             </div>
         </div>
 
